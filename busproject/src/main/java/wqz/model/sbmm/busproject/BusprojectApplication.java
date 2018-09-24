defda8792fe3eabfcb4c3da10cba8a9e46dc5f4a
wuqingzhou <wuqingzhou1989@yeah.net>
2018-09-24 14:18:04 +0800

    格式化

--- busproject/src/main/java/wqz/model/sbmm/busproject/BusprojectApplication.java
+++ busproject/src/main/java/wqz/model/sbmm/busproject/BusprojectApplication.java
@@ -11,7 +11,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @MapperScan(basePackages = "wqz.model.sbmm.busproject.mapper")
 public class BusprojectApplication {
 
-	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(BusprojectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(BusprojectApplication.class, args);
+    }
 }
